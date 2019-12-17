7fc3c5f055406263255b8c7c87ad5d61da2b9179
haqhuy99 <haqhuy99.wd@gmail.com>
2019-12-17 22:45:07 +0700

    edit formrequest of student

--- app/Http/Requests/Students/CreateStudentRequest.php
+++ app/Http/Requests/Students/CreateStudentRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Students;
 
+use App\Rules\UpperCase;
 use Illuminate\Foundation\Http\FormRequest;
 
 class CreateStudentRequest extends FormRequest
@@ -13,7 +14,7 @@ class CreateStudentRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +25,30 @@ class CreateStudentRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name'=>['required','unique:students,name',new UpperCase()],
+            'birthday'=>'required',
+            'image'=>'required|image|mimes:jpeg,bmp,png',
+            'phone'=>'required|regex:/\d{3}-\d{4}-\d{3}/',
+            'gender'=>'required',
+            'address'=>'required',
+            'classrooms'=>'required|array|min:1',
+
+        ];
+    }
+    public  function  messages()
+    {
+        return [
+            'name.required'=>'* Tên sinh viên không được để trống',
+            'name.unique'=>'* Tên sinh viên đã tồn tại! Hãy thử lại với tên khác',
+            'image.required'=>'* Ảnh đại diện là bắt buộc',
+            'image.image'=>'* Ảnh đại diện phải là hình ảnh',
+            'image.mines'=>'* Ảnh đại diện phỉa là 1 trong các định dạng JPG,PNG,BMP',
+            'phone.required'=>'* Số điện thoại không để trống',
+            'phone.regex'=>'* Số điện thoại có dạng XXX-XXXX-XXX',
+            'gender.required'=>'* Chọn giới tính',
+            'address.required'=>'Địa chỉ không để trống',
+            'birthday.required'=>'Ngày sinh nhật không để trống',
+            'classrooms.required'=>'* Chọn lớp ',
         ];
     }
 }
--- app/Http/Requests/Students/UpdateStudentRequest.php
+++ app/Http/Requests/Students/UpdateStudentRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Students;
 
+use App\Rules\UpperCase;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateStudentRequest extends FormRequest
@@ -13,7 +14,7 @@ class UpdateStudentRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +25,26 @@ class UpdateStudentRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name'=>['required','unique:students,name,'.$this->student,new UpperCase()],
+            'birthday'=>'required',
+            'phone'=>'required|regex:/\d{3}-\d{4}-\d{3}/',
+            'gender'=>'required',
+            'address'=>'required',
+            'classrooms'=>'required|array|min:1',
+
+        ];
+    }
+    public  function  messages()
+    {
+        return [
+            'name.required'=>'* Tên không được để trống',
+            'name.unique'=>'* Tên đã tồn tại! Hãy thử lại với tên khác',
+            'phone.required'=>'* Số điện thoại không để trống',
+            'phone.regex'=>'* Số điện thoại có dạng XXX-XXXX-XXX',
+            'gender.required'=>'* Chọn giới tính',
+            'address.required'=>'Địa chỉ không để trống',
+            'birthday.required'=>'Ngày sinh nhật không để trống',
+            'classrooms.required'=>'* Chọn lớp ',
         ];
     }
 }
