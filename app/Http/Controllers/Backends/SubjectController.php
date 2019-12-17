75f34e7b4aedb38a8b6a5364fead2ba6cb8271fa
haqhuy99 <haqhuy99.wd@gmail.com>
2019-12-17 14:25:51 +0700

    edit file request of subject

--- app/Http/Controllers/Backends/SubjectController.php
+++ app/Http/Controllers/Backends/SubjectController.php
@@ -37,7 +37,7 @@ class SubjectController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store( $request)
     {
         //
     }
--- app/Http/Requests/Subjects/UpdateSubjectRequest.php
+++ app/Http/Requests/Subjects/UpdateSubjectRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests\Subjects;
 
+use App\Rules\ShortStringLengt;
+use App\Rules\UpperCase;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateSubjectRequest extends FormRequest
@@ -13,7 +15,7 @@ class UpdateSubjectRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +26,23 @@ class UpdateSubjectRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => ['required', 'unique:subjects,name,'.$this->subject, new ShortStringLengt(), new UpperCase()],
+            'description' => 'required',
+            'lesson' => 'required|numeric|max:100|min:15',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => '* Tên môn học không được để trống',
+            'name.unique' => '* Tên môn học đã có ! Hãy thử tên khác',
+            'description.required' => '* Mô tả không được để trống',
+            'lesson.required' => '* Số tiết môn học không được để trống',
+            'lesson.numeric' => '* Số tiết phải là số',
+            'lesson.max' => '* Số tiết phải nhỏ hơn 100',
+            'lesson.min' => '* Số tiết phải lớn hơn 15',
+
         ];
     }
 }
