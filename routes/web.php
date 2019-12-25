73bfd2a44fcd574bc21b8797dbd984cf49c4e709
haqhuy99 <haqhuy99.wd@gmail.com>
2019-12-25 14:14:58 +0700

    user middelware to route

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::group(['prefix' => 'manage', 'namespace' => 'Admins', 'middleware' => ['auth']], function () {
+Route::group(['prefix' => 'manage', 'namespace' => 'Admins', 'middleware' => ['auth','check.user']], function () {
     Route::get('/', 'DashboardController@index')->name('dashboard.index');
     Route::resource('/users', 'UserController')->except([
         'update',
