93c3d2d8f93bafb90fd8addb517e56ae8f17a87b
haqhuy99 <haqhuy99.wd@gmail.com>
2019-12-25 09:52:29 +0700

    add logout function to login controoelr

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,4 +36,11 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        return redirect()->route('login');
+    }
+
 }
