dbea912120980747d724bf83ccf7d3026bde2b8a
haqhuy99 <haqhuy99.wd@gmail.com>
2019-12-25 09:35:47 +0700

    fix somme error when change function to curd user

--- public/js/script.js
+++ public/js/script.js
@@ -7,243 +7,209 @@ $(function () {
     });
     //count row table
     countStt();
+
     //variable
     let idActionResource = 0;
     let urlResource = "";
     let dataResource = "";
     let searchPath = "";
-    let searchMessage = $('.search-message');
+    let getMethodForm='get';
+    let newMethodForm='post';
     //faculty
-    //valiable
+    //variable
     let faculty = $('#faculty');
-    let newFacultyForm = $('.newfaculty-form');
-    let editFacultyForm = $('.editfaculty-form');
     let addFacultyBtn = $('.add-faculty');
-    let editFacultyBtn = $('.edit-faculty');
-    let facultyPath = "/manage/faculties/";
-    let facultySearch = $('.faculty-searchkey');
-
+    let facultyPath = "/manage/faculties";
     // function
-    function resetError() {
-        $('.nameError').html('');
-        $('.descriptionError').html('');
-    }
-
-    function showErrorFaculty(errors) {
-        (errors.name) ? $('.nameError').html(errors.name[0]) : "";
-        (errors.description) ? $('.descriptionError').html(errors.description[0]) : "";
-    }
-
     addFacultyBtn.click(function () {
-        resetError();
-    })
-    editFacultyBtn.click(function (event) {
-        idActionResource = $(this).attr('editId');
-        resetError();
+        resetErrorFaculty();
+    });
+    faculty.on('click', '.edit-faculty', function () {
+        resetErrorFaculty();
+        idActionResource=$(this).attr('editId');
+        urlResource=facultyPath+"/"+idActionResource;
+        callAjax(urlResource,null,getMethodForm)
+            .done(data => {
+                let faculty=data.data;
+                $('.faculty-name').val(faculty.name);
+                $('.faculty-description').val(faculty.description);
+            })
     });
 
     faculty.on('click', '.new-faculty', function () {
-        dataResource = newFacultyForm.serialize();
-        urlResource = '/manage/faculties';
-        newResource(dataResource, urlResource)
-            .done(response => {
+        dataResource = new FormData($('.new-faculty-form')[0]);
+        callAjax(facultyPath,dataResource,newMethodForm)
+            .done(data => {
                 $('#newFacultyModal').modal('hide')
-                isSuccess(response.status) ? alertSuccess(response.message) : "";
+                alertSuccess(data.message);
                 countStt();
             })
-            .fail(error => {
-                const errors = error.responseJSON.errors;
+            .fail(data => {
+                const errors = data.responseJSON.errors;
+                resetErrorFaculty();
                 showErrorFaculty(errors);
             });
     });
     faculty.on('click', '.update-faculty', function () {
-        dataResource = editFacultyForm.serialize();
-        urlResource = facultyPath + idActionResource;
-        updateResource(dataResource, urlResource)
-            .done(response => {
-                $('#newFacultyModal').modal('hide')
-                isSuccess(response.status) ? alertSuccess(response.message) : "";
+        dataResource = new FormData($('.edit-faculty-form')[0]);
+        urlResource = facultyPath + '/update/' + idActionResource;
+        callAjax(urlResource,dataResource,newMethodForm)
+            .done(data => {
+                $('#newFacultyModal').modal('hide');
+                alertSuccess(data.message);
                 countStt();
             })
-            .fail(error => {
-                const errors = error.responseJSON.errors;
+            .fail(data => {
+                const errors = data.responseJSON.errors;
                 showErrorFaculty(errors);
             });
     });
-
     faculty.on('click', '.delete-faculty', function () {
         idActionResource = $(this).attr('deleteId');
-        destroyResource(idActionResource, facultyPath);
+        urlResource = facultyPath + "/"+idActionResource;
+        destroyResource(urlResource);
     });
 
-    facultySearch.on('keyup', function () {
-        let searchKey = $(this).val().trim();
-        searchPath = facultyPath + "search/" + searchKey;
-        if (searchKey.length > 0) {
-            searchResource(searchPath)
-                .done(data => {
-                    $('.pagination-container').hide();
-                    const faculties = data.data;
-                    searchMessage.html(data.message);
-                    let facutyTable = fillFacultyToTableHtml(faculties);
-                    $('tbody').html(facutyTable);
-                    countStt();
-                })
-                .fail(error => {
-                    searchMessage.html('');
-                });
-        } else {
-            searchMessage.html('');
-        }
-        facultySearch.on('keydown', function () {
-            if (searchKey.length < 0) {
-                searchMessage.html('');
-            }
-        });
-
-    });
     //fill data to modal
-    $('#editModal').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget)
-        var name = button.data('name');
-        var description = button.data('description');
-        var modal = $(this);
-        modal.find('.modal-body .name').val(name);
-        modal.find('.modal-body .description').val(description);
-    });
     //subject
-    //valiable
+    //variable
     let subject = $('#subject');
-    let newSubjectForm = $('.newsubject-form');
-    let editSubjectForm = $('.editsubject-form');
     let addSubjectBtn = $('.add-subject');
     let editSubjectBtn = $('.edit-subject');
-    let subjectPath = "/manage/subjects/";
-    let subjectSearch = $('.subject-searchkey');
-
+    let subjectPath = "/manage/subjects";
     // function
-    function resetErrorSubject() {
-        $('.nameError').html('');
-        $('.lessonError').html('');
-        $('.descriptionError').html('');
-    }
-
-    function showErrorFacultySubject(errors) {
-        (errors.name) ? $('.nameError').html(errors.name[0]) : "";
-        (errors.name) ? $('.lessonError').html(errors.lesson[0]) : "";
-        (errors.description) ? $('.descriptionError').html(errors.description[0]) : "";
-    }
-
     addSubjectBtn.click(function () {
         resetErrorSubject();
-    })
-    editSubjectBtn.click(function (event) {
-        idActionResource = $(this).attr('editId');
-        showErrorFacultySubject();
     });
-
     subject.on('click', '.new-subject', function () {
-        dataResource = newSubjectForm.serialize();
-        urlResource = '/manage/subjects';
-        newResource(dataResource, urlResource)
-            .done(response => {
+        dataResource = new FormData($('.new-subject-form')[0]);
+        callAjax(subjectPath,dataResource,newMethodForm)
+            .done(data => {
                 $('#newsubjectModal').modal('hide');
-                isCreated(response.status) ? alertSuccess(response.message) : "";
+                alertSuccess(data.message);
                 countStt();
             })
-            .fail(error => {
-                const errors = error.responseJSON.errors;
-                showErrorFacultySubject(errors);
+            .fail(data => {
+                const errors = data.responseJSON.errors;
+                resetErrorSubject();
+                showErrorSubject(errors);
             });
+    });
+    subject.on('click', '.edit-subject', function () {
+        resetErrorSubject();
+        idActionResource=$(this).attr('editId');
+        urlResource=subjectPath+"/"+idActionResource;
+        callAjax(urlResource,null,getMethodForm)
+            .done(data => {
+                let subject=data.data;
+                $('.subject-name').val(subject.name);
+                $('.subject-lesson').val(subject.lesson);
+                $('.subject-description').val(subject.description);
+            })
     });
     subject.on('click', '.update-subject', function () {
-        dataResource = editSubjectForm.serialize();
-        urlResource = subjectPath + idActionResource;
-        updateResource(dataResource, urlResource)
-            .done(response => {
-                $('#newsubjectModal').modal('hide')
-                isSuccess(response.status) ? alertSuccess(response.message) : "";
+        dataResource = new FormData($('.edit-subject-form')[0]);
+        urlResource = subjectPath + "/update/" + idActionResource;
+        callAjax(urlResource,dataResource,newMethodForm)
+            .done(data => {
+                $('#editSubjectModal').modal('hide')
+                alertSuccess(data.message);
                 countStt();
             })
-            .fail(error => {
-                const errors = error.responseJSON.errors;
-                showErrorFaculty(errors);
+            .fail(data => {
+                const errors = data.responseJSON.errors;
+                resetErrorSubject();
+                showErrorSubject(errors);
             });
     });
 
-    subject.on('click', '.delete-faculty', function () {
+    subject.on('click', '.delete-subject', function () {
         idActionResource = $(this).attr('deleteId');
-        destroyResource(idActionResource, facultyPath);
+        urlResource=subjectPath + "/"+idActionResource;
+        destroyResource(urlResource);
     });
 
-    subjectSearch.on('keyup', function () {
-        let searchKey = $(this).val().trim();
-        searchPath = subjectPath + "search/" + searchKey;
-        if (searchKey.length > 0) {
-            searchResource(searchPath)
-                .done(data => {
-                    $('.pagination-container').hide();
-                    const subjects = data.data;
-                    console.log(subjects);
-                    searchMessage.html(data.message);
-                    let subjectTable = fillSubjectToTableHtml(subjects);
-                    $('tbody').html(subjectTable);
-                    countStt();
-                })
-                .fail(error => {
-                    searchMessage.html('');
-                });
-        } else {
-            searchMessage.html('');
-        }
-        subjectSearch.on('keydown', function () {
-            if (searchKey.length < 0) {
-                searchMessage.html('');
-            }
-        });
+//classroom
+    $('.classroom-select-faculty').select2();
+    $('.classroom-select-subject').select2();
 
+    let classroom = $('#classroom');
+    let classroomPath = "/manage/classrooms";
+
+    classroom.on('click', '.delete-classroom', function () {
+        idActionResource = $(this).attr('deleteId');
+        urlResource=classroomPath+"/"+idActionResource;
+        destroyResource(urlResource);
     });
-    //fill data to modal
-    $('#editSubjectModal').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget)
-        var name = button.data('name');
-        var description = button.data('description');
-        var lesson = button.data('lesson');
-        var modal = $(this);
-        modal.find('.modal-body .name').val(name);
-        modal.find('.modal-body .description').val(description);
-        modal.find('.modal-body .lesson').val(lesson);
+    classroom.on('click','.show-classroom',function () {
+        idActionResource=$(this).attr('showId');
+        urlResource=classroomPath+"/"+idActionResource;
+        callAjax(urlResource)
+            .done(data => {
+                let classroom = data.data;
+                $('.classroom-name').html(classroom.name);
+                $('.classroom-description').html(classroom.description);
+                $('.classroom-member').html(classroom.member);
+                $('.classroom-faculty').html(classroom.faculty.name);
+                $('.classroom-subject').html(classroom.subject.name);
+            })
+    });
+
+    //student
+
+    let student = $('#student');
+    let studentPath = "/manage/students/";
+    $('.student-select-classroom').select2();
+    student.on('click', '.delete-student', function () {
+        idActionResource = $(this).attr('deleteId');
+        urlResource=studentPath+idActionResource;
+        destroyResource(urlResource);
+    });
+
+    student.on('click', '.show-student', function () {
+        idActionResource = $(this).attr('showId');
+        urlResource=studentPath+idActionResource;
+        callAjax(urlResource)
+            .done(data=>{
+                let student=data.data;
+                $('.student-name').html(student.name);
+                $('.student-address').html(student.address);
+                $(' .student-gender').html((student.gender == 0) ? 'Nam' : 'Nữ');
+                $(' .student-image').attr('src', '/images/students/' + student.image);
+                let classrooms = (student.classrooms.length > 0) ? arrayOjectParseToNameP(student.classrooms) : 'Không có';
+                $('.student-classroom').html(classrooms);
+                $(' .student-birthday').html(student.birthday);
+                $('.student-phone').html(student.phone);
+
+            })
     });
     //user
     let user = $('#user');
     let addUserBtn = $('.add-user');
-    let userPath = "/manage/users/";
-    let userSearch = $('.user-searchkey');
+    let userPath = "/manage/users";
     // function
     addUserBtn.click(function () {
         resetErrorUser();
     });
     user.on('click', '.new-user', function () {
         dataResource = new FormData($('#new-user-form')[0]);
-        urlResource = '/manage/users';
-        newResource(dataResource, urlResource)
+        callAjax(urlResource,dataResource)
             .done(data => {
-                 $('#newUserModal').modal('hide');
-                console.log(data);
-               isCreated(data.status) ? alertSuccess(data.message) : "";
+                $('#newUserModal').modal('hide');
+                alertSuccess(data.message);
                 countStt();
             })
             .fail(data => {
                 const errors = data.responseJSON.errors;
-                console.log(errors);
+                resetErrorUser();
                 showErrorUser(errors);
             });
     });
     user.on('click', '.edit-user', function () {
         resetErrorUser();
         idActionResource = $(this).attr('editId');
-        urlResource = userPath + idActionResource;
-        getResource(urlResource)
+        urlResource =userPath+"/"+ idActionResource;
+        callAjax(urlResource)
             .done(response => {
                 let user = response.data;
                 $('.user-name').val(user.name);
@@ -256,51 +222,49 @@ $(function () {
 
     $('body').on('click', '.show-user', function () {
         idActionResource = $(this).attr('showId');
-        urlResource = userPath + idActionResource;
-        getResource(urlResource)
+        urlResource = userPath+'/'+ idActionResource;
+        callAjax(urlResource)
             .done(response => {
                 let user = response.data;
                 $('.user-name').html(user.name);
-                $('.user-email').html(user.email);
+                $('.user-email').html(user.email);s
                 $('.user-phone').html(user.phone);
                 $('.user-image').attr('src', '/images/users/' + user.image);
                 let role = "người dùng";
-                if (user.role == 1) {
+                if (user.role == 2) {
                     role = "nhân viên";
                 }
-                if (user.role == 2) {
+                if (user.role == 3) {
                     role = "Quản trị";
                 }
-
                 $('.user-role').html(role);
             })
     });
     user.on('click', '.update-user', function () {
         dataResource = new FormData($('#update-user-form')[0]);
-        console.log(dataResource);
-        urlResource ='/manage/users/update/'+idActionResource;
-        newResource(dataResource,urlResource)
-        .done(response => {
+        urlResource = userPath+'/update/' + idActionResource;
+        callAjax( urlResource,dataResource,'post')
+            .done(response => {
                 $('#editUserModal').modal('hide')
-                isSuccess(response.status) ? alertSuccess(response.message) : "";
-                 countStt();
+                alertSuccess(response.message);
+                countStt();
             })
             .fail(error => {
-                console.log(error);
                 const errors = error.responseJSON.errors;
-                 showErrorUser(errors);
+                showErrorUser(errors);
             });
     });
 
     user.on('click', '.delete-user', function () {
         idActionResource = $(this).attr('deleteId');
-        destroyResource(idActionResource, userPath);
+        urlResource=userPath+"/"+idActionResource
+        destroyResource(urlResource);
     });
 
     user.on('click', '.set-password', function () {
-        dataResource = $('#set-password-form').serialize();
-        urlResource = userPath + "setpassword/" + idActionResource;
-        updateResource(dataResource, urlResource)
+        dataResource = new FormData($('#set-password-form')[0]);
+        urlResource = userPath + "set-password/" + idActionResource;
+        callAjax( urlResource,dataResource,'post')
             .done(response => {
                 $('#changePasswordModal').modal('hide')
                 alertSuccess(response.message, 1);
@@ -308,49 +272,27 @@ $(function () {
             })
             .fail(error => {
                 const errors = error.responseJSON.errors;
+                resetErrorUser();
                 showErrorUser(errors);
             });
     });
-        //change password
     $('body').on('click', '.change-password', function () {
-        dataResource = $('#change-password-form').serialize();
-        urlResource = userPath + "changepassword/" + idActionResource;
-        updateResource(dataResource, urlResource)
-            .done(response => {
-                $('#changeUserPasswordModal').modal('hide')
-                alertSuccess(response.message, 1);
-                countStt();
+        dataResource = new FormData($('#change-password-form')[0]);
+        urlResource = userPath + "change-password/" + idActionResource;
+        callAjax(urlResource,dataResource,'post')
+            .done(data => {
+                console.log(data);
+                //   $('#changePasswordModal').modal('hide')
+                // alertSuccess(response.message, 1);
+                //   countStt();
             })
-            .fail(error => {
-                const errors = error.responseJSON.errors;
-                showErrorUser(errors);
-            });
-    });
+            .fail(data => {
+                const errors = data.responseJSON.errors;
+                resetErrorPassword();
+                showErrorPassword(errors);
 
-    // userSearch.on('keyup', function () {
-    //     let searchKey = $(this).val().trim();
-    //     searchPath = facultyPath + "search/" + searchKey;
-    //     if(searchKey.length>0){
-    //         searchResource(searchPath)
-    //             .done(data => {
-    //                 $('.pagination-container').hide();
-    //                 const faculties = data.data;
-    //                 searchMessage.html(data.message);
-    //                 let facutyTable = fillFacultyToTableHtml(faculties);
-    //                 $('tbody').html(facutyTable);
-    //                 countStt();
-    //             })
-    //             .fail(error => {
-    //                 searchMessage.html('');
-    //             });
-    //     }else {
-    //         searchMessage.html('');
-    //     }
-    //     facultySearch.on('keydown',function () {
-    //         if(searchKey.length<0){
-    //             searchMessage.html('');
-    //         }
-    //     });
-    //
-    // });
+            });
+    })
+
+
 });
