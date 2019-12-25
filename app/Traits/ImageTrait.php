adb9cbf3f2254e7cade19f70238755e9e83289d1
haqhuy99 <haqhuy99.wd@gmail.com>
2019-12-25 09:40:56 +0700

    update image trait

--- app/Traits/ImageTrait.php
+++ app/Traits/ImageTrait.php
@@ -1,39 +1,45 @@
 <?php
+
 namespace App\Traits;
+
 use Image;
-trait ImageTrait{
-    public  function veryfyImage($request,$filename='image'){
-        if($request->hasFile($filename)){
-                return true;
+
+trait ImageTrait
+{
+    public function veryfyImage($image, $filename = 'image')
+    {
+        if ($image->hasFile($filename)) {
+            return true;
         }
         return false;
-
-    }
-    public function saveImage($request,$path,$filename='image')
-    {
-            if($this->veryfyImage($request,$filename)){
-                $name = time() . '.' . $request->file($filename)->getClientOriginalExtension();
-                Image::make($request->file($filename))->resize(300, 300)->save($path . $name);
-                return $name;
-            }
-
     }
 
-    public function deleteImage($name)
+    public function saveImage($image, $path, $filename = 'image')
     {
-        if (file_exists($this->imagePath . $name) && $name != 'default.jpg') {
-            unlink($this->imagePath . $name);
+        if ($this->veryfyImage($image, $filename)) {
+            $name = time() . '.' . $image->file($filename)->getClientOriginalExtension();
+            Image::make($image->file($filename))->resize(300, 300)->save($path . $name);
+            return $name;
+        }
+
+    }
+
+    public function deleteImage($name, $path)
+    {
+        if (file_exists($path . $name) && $name != 'default.jpg') {
+            unlink($path . $name);
         }
     }
 
-    public function updateImage($request,$path, $currentName,$filename='image')
+    public function updateImage($image, $path, $currentName, $filename = 'image')
     {
-        if ($request) {
-            $name = $this->saveimage($request,$path,$filename);
-            $this->deleteimage($currentName);
+        if ($this->veryfyImage($image, $filename)) {
+            $name = $this->saveimage($image, $path, $filename);
+            $this->deleteImage($currentName, $path);
             return $name;
         } else {
             return $currentName;
         }
     }
+
 }
