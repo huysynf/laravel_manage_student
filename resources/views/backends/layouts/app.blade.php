24eb3d82bd54fe1902b5326609ed55f3f006c0fb
haqhuy99 <haqhuy99.wd@gmail.com>
2019-12-26 22:02:30 +0700

    fix error sweate aleart 2 not show icon

--- resources/views/backends/layouts/app.blade.php
+++ resources/views/backends/layouts/app.blade.php
@@ -94,8 +94,6 @@
 <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
 
 <!-- Core plugin JavaScript-->
-<script src="{{asset('js/jquery/jquery.easing.min.js')}}"></script>
-
 <!-- Custom scripts for all pages-->
 <script src="{{asset('js/sb-admin-2.min.js')}}"></script>
 <script src="{{asset('js/sweetalert2.min.js')}}"></script>
