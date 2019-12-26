130a2a1ce274ce302558d9c4cfe9d28906048363
haqhuy99 <haqhuy99.wd@gmail.com>
2019-12-26 09:17:16 +0700

    fix some error function when curd facuty

--- public/js/function.js
+++ public/js/function.js
@@ -1,4 +1,4 @@
-function alertSuccess(message, reload = 0) {
+function alertSuccess(message) {
     Swal.fire({
         position: 'center',
         icon: 'success',
@@ -8,9 +8,7 @@ function alertSuccess(message, reload = 0) {
     })
         .then((result) => {
             if (result.value) {
-                if (reload == 0) {
-                    location.reload();
-                }
+
             }
         })
 }
@@ -34,27 +32,29 @@ function callAjax( url,data="",type='get') {
         contentType: false,
     });
 }
-function destroyResource(url) {
-    Swal.fire({
-        title: 'Xác nhận xóa?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Xóa ',
-        cancelButtonText: 'Hủy bỏ'
-    }).then((result) => {
-        if (result.value) {
-            callAjax(url,null,'delete')
-                .done(response => {
-                    alertSuccess(response.message);
-                })
-                .fail(error => {
-                    alertError(error.message);
-                });
-        }
-    });
-}
+destroyResource = (url) => {
+    return new Promise(((resolve, reject) => {
+        Swal.fire({
+            title: 'Xác nhận xóa?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Xóa ',
+            cancelButtonText: 'Hủy bỏ'
+        }).then((result) => {
+            if (result.value) {
+                callAjax(url, null, 'delete')
+                    .done(data => {
+                        resolve(data);
+                    })
+                    .fail(data => {
+                        reject(data);
+                    });
+            }
+        });
+    }))
+};
 
 //count row table
 function countStt() {
@@ -74,4 +74,25 @@ function resetErrorFaculty() {
 function showErrorFaculty(errors) {
     (errors.name) ? $('.nameError').html(errors.name[0]) : "";
     (errors.description) ? $('.descriptionError').html(errors.description[0]) : "";
+}
+function  fillFacultyToRowTable(faculty) {
+    return ` <td>
+                <strong></strong>
+            </td>
+            <td>
+                ${faculty.name}
+            </td>
+            <td>${faculty.description}</td>
+            <td>
+                <button class="btn btn-outline-primary btn-circle edit-faculty"
+                        title="Cập nhật thông tin khoa"
+                        editId="${faculty.id}"
+                        data-toggle="modal"
+                        data-target="#editFacultyModal"
+                ><i class="fa fa-edit text-warning"></i>
+                </button>
+                <button class="btn btn-outline-dark delete-faculty btn-circle" title="Xóa nhật khoa"
+                        deleteId="${faculty.id}"><i class="fas fa-trash text-danger"></i></button>
+            </td>`;
+
 }
