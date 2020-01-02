b0a3273511f2ecdda52e5d3af834613b34c4c417
haqhuy99 <haqhuy99.wd@gmail.com>
2020-01-02 20:48:02 +0700

    create controlelr moddel table view of classroom schudule

---
+++ app/Http/Controllers/Admins/ClassroomScheduleController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admins;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ClassroomScheduleController extends Controller
+{
+    public function index($id){
+
+    }
+    public function store(Request $request){
+
+    }
+
+    public function destroy($id){
+
+    }
+
+
+}
